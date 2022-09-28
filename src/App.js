fdc5c7e9119be83fc4e8330ece8178b7bcfde345
2Nadim <nadimmahamud661@gmail.com>
2022-09-28 17:42:14 +0100

    header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <Header></Header>
 
     </div>
   );
 }
+function Header() {
+  return (
+    <div>
+      <h1>My Daily Routine</h1>
 
+    </div>
+  )
+}
 export default App;
