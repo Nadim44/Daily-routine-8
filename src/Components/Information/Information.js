e0e153c9131d8dd9e10abdb34e17285e26b2e4d6
2Nadim <nadimmahamud661@gmail.com>
2022-09-28 22:12:19 +0100

    personal information added

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -8,16 +8,37 @@ const Information = (props) => {
     for (const subject of time) {
         totalTime = totalTime + subject.time
     }
+    const breakTimeHandler = () => {
+        console.log('clicked')
+
+    }
+
     return (
         <div className='information'>
             <div >
-                <p>Nadim Mahamud</p>
+                <div>
+                    <h3>Nadim Mahamud</h3>
+                    <p>Narsingdi,Dhaka</p>
+                </div>
+                <p style={{ padding: '10px' }}>My name is Nadim. I did my Diploma course in computer From Narsingdi Polytechnic Institute. Currently i pursuing BSC in CSE at Eastern University.</p>
+            </div>
+            <div>
+                <h2>Add A Break</h2>
+
+                <button className='btn' onClick={breakTimeHandler}>30 <small>min</small></button>
+                <button>40<small>min</small></button>
+                <button>50<small>min</small></button>
+
             </div>
             <div>
                 <h2>Time Details</h2>
                 {/* <h4>Study Time: {time.length}</h4> */}
                 <h4 style={{ backgroundColor: 'gray', padding: '5px' }}>Study Time: {totalTime} <small>minutes</small></h4>
+                <h4 id='break-time'>Break Time: 0 <small>minutes</small></h4>
             </div>
+            <button style={{ backgroundColor: 'orange' }}>
+                <h2>Activity Completed</h2>
+            </button>
         </div>
     );
 };
